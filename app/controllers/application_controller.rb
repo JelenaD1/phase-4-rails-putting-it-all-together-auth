ff8a940cda6b4ef3e6d34b407cd5e8c88dc36a61
Jelena <jelena.durovic@powerhrg.com>
2021-09-17 13:27:53 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def render_unprocessable_entity(error)
+    render json: { errors: error.record.errors.full_messages }, status: :unprocessable_entity
+
+  end 
+
 end
